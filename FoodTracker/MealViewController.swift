737e759adc8bcee9a8b1e2bf18992e2e3d8eca0d
k-andy <andy.sh182@gmail.com>
2016-03-16 11:09:31 +0100

    final commit

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -19,12 +19,24 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
         // Enable the Save button only if the text field has a valid Meal name.
         checkValidMealName()
     }
     // MARK: Navigation
     @IBAction func cancel(sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
         dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            navigationController!.popViewControllerAnimated(true)
+        }
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if saveButton === sender {
